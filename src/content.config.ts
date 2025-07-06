0fe4f3f11d5aeaf8f2746529c7accd92d8cf0b0d
randompatternsjk <me@jordankeller.work>
2025-07-06 13:34:18 -0400

    Updating dynamic routing for slug and simplifying component structure to remove ProjectHeader

--- src/content.config.ts
+++ src/content.config.ts
@@ -4,11 +4,13 @@ const projects = defineCollection({
   schema: z.object({
     title: z.string(),
     description: z.string(),
+    pubDate: z.coerce.date(),
     heroImage: z.string().optional(),
     featuredVideo: z.string().optional(),
-    pubDate: z.coerce.date(),
-    duties: z.array(z.string()).optional(),
     type: z.string().optional(),
+    category: z.string().optional(),
+    duties: z.array(z.string()).default([]), // Instead of .optional()
+    color: z.string().optional(),
   }),
 });
 
